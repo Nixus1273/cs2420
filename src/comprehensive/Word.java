d356c678ca7cca606964f8213649ca4036fe64f9
Nixus1273 <flickbrand26@gmail.com>
2024-11-26 15:11:10 -0700

    Meeting on teusday 11/26

--- src/comprehensive/Word.java
+++ src/comprehensive/Word.java
@@ -4,26 +4,31 @@ import java.util.*;
 
 public class Word {
 
-    private String word;
+    private final String word;
     private TreeMap<Definition, Definition> definitions = new TreeMap<>(new DefinitionComparator());
+    ArrayList<String> validPOS = new ArrayList<>(Arrays.asList(
+            "adj", "adv", "conj", "interj", "noun", "prep", "pron", "verb"));
+    Integer[] POSCount = new Integer[]{0, 0, 0, 0, 0, 0, 0, 0};
 
     public Word (String word, String POS, String def) {
         this.word = word;
-        Definition d = new Definition(POS, def);
-        this.definitions.put(d, d);
+        addDefinition(POS, def);
     }
 
-    public Collection<Definition> getDefinitions() {
-        return definitions.values();
+
+    public ArrayList<Definition> getDefinitions() {
+        return new ArrayList<>(definitions.values());
     }
 
     public void removeDefinition(Definition d) {
+        //Get the pos of the def and -1 from the hash
+        POSCount[validPOS.indexOf(d.getPOS())] -= 1;
         definitions.remove(d);
     }
 
     public void updateDefinition(Definition oldDef, String newDef) {
         String oldPOS = definitions.get(oldDef).getPOS();
-        definitions.remove(oldDef);
+        removeDefinition(oldDef);
         Definition d = new Definition(oldPOS, newDef);
         this.definitions.put(d, d);
     }
@@ -31,17 +36,29 @@ public class Word {
     public void addDefinition(String POS, String def) {
         Definition d = new Definition(POS, def);
         this.definitions.put(d, d);
+        POSCount[validPOS.indexOf(POS)] += 1;
     }
 
     public String getWord() {
         return this.word;
     }
 
+    public String getAllPos() {
+        String allPOS = "";
+        for (int i = 0; i < POSCount.length; i++) {
+            if (POSCount[i] > 0){
+                allPOS +=  "\t" + validPOS.get(i) + ".\n";
+            }
+        }
+        return allPOS;
+    }
 
-    public class Definition {
+    public int numberOfDefinitions() {
+        return definitions.size();
+    }
 
-        private final ArrayList<String> validPOS = new ArrayList<>(
-                Arrays.asList("noun", "verb", "adj", "adv", "pron", "prep", "conj", "interj"));
+    public static class Definition {
+
         private String POS;
         private String def;
 
@@ -56,22 +73,6 @@ public class Word {
 
         public String getDef() {
             return this.def;
-        }
-
-        public void setPOS(String POS) throws IllegalArgumentException {
-            if(validPOS.contains(POS))
-                this.POS = POS;
-
-            throw new IllegalArgumentException();
-
-            //Resort the PQ
-        }
-
-
-        public void setDef(){
-
-
-            //Resort the PQ
         }
     }
 }
